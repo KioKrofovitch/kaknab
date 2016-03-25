748398da50cca71a055fa7017d80111367fa2f8f
Kio Krofovitch <kio.krofovitch@gmail.com>
2016-03-25 14:35:14 -0600

    Beaches added and opening maps, not quite split

--- app/src/main/java/com/kiodev/kaknab/MainActivity.java
+++ app/src/main/java/com/kiodev/kaknab/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kiodev.kaknab;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickTulum(View view){
+        Uri tulumUri = Uri.parse("google.streetview:cbll=20.2152725,-87.4287996");
+        launchMapView(tulumUri);
+    }
+
+    public void onClickAkumal(View view){
+        Uri akumalUri = Uri.parse("google.streetview:cbll=20.3959299,-87.3100734");
+        launchMapView(akumalUri);
+    }
+
+    public void onClickXpuHa(View view){
+        Uri xpuHaUri = Uri.parse("google.streetview:cbll=20.4683153,-87.2604617");
+        launchMapView(xpuHaUri);
+    }
+
+    public void onClickXelHa(View view){
+        Uri xelHaUri = Uri.parse("google.streetview:cbll=20.4347062,-87.2862159");
+        launchMapView(xelHaUri);
+    }
+    private void launchMapView(Uri locUri){
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, locUri);
+        mapIntent.setFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
     }
 }
